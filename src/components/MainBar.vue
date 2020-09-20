27104baf4d537477695371e7f4fd24af2532a4e0
acatzk <joshuaimalay@gmail.com>
2020-09-20 16:39:23 +0800

    Added layout for the main bar ang added name or each slots

---
+++ src/components/MainBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="w-1/2 h-full overflow-y-scroll">
+		<div class="px-5 py-3 border-b border-gray-300 flex items-center justify-between">
+			<slot name="home-title"></slot>
+		</div>
+		<div class="px-5 py-3 border-b-8 border-gray-300 flex">
+			<slot name="tweet-field"></slot>
+		</div>
+		<div class="flex flex-col-reverse">
+			<slot name="tweet-user-fresh-post"></slot>
+		</div>
+		<slot name="tweet-posts"></slot>
+	</div>
+</template>
