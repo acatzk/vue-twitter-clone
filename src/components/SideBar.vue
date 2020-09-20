90e6215315b211bd1f6ebf3f7cae3da7bb633b1a
acatzk <joshuaimalay@gmail.com>
2020-09-20 12:34:29 +0800

    Added layout for sidebar and passed slot thru it

---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="lg:w-1/5 border-r px-2 lg:px-6 py-2 flex flex-col justify-between">
+		<div>
+			<slot name="twitter-logo"></slot>
+			<div>
+				<slot name="twitter-tabs"></slot>
+			</div> <!-- NAIGATION LINKS -->
+			<slot name="twitter-button"></slot>
+		</div>
+		<div class="w-full relative">
+			<slot name="twitter-avatar"></slot>
+		</div>
+	</div> <!-- END SIDE BAR -->
+</template>
